39057b505149b2d4419beaf6c54a5d3b6d68f1f3
Holly Smith <hs1526@york.ac.uk>
2020-10-22 16:02:39 +0100

    imported, summarised and analysed data

---
+++ chaff.R
@@ -0,0 +1,25 @@
+
+#import data
+
+chaff <- read.table("raw_data/chaff.txt", header = T)
+
+#tidy data
+
+library(tidyverse)
+
+chaff2 <- gather(chaff, key = sex, mass)
+
+#summarise data
+
+chaff_sum <- chaff2 %>% 
+  group_by(sex) %>% 
+  summarise(mean = mean(mass),
+            n = length(mass),
+            std= sd(mass),
+            se = std/sqrt(n))
+#analyse data
+
+anova <- aov(mass ~ sex, data = chaff2)
+summary(anova)
+
+#there is a sig diff in mass btw sexes
